1388395aa6df3d63e1c52ae7f069a7afee0280e8
Chris Van Law <chris@chrisvanlaw.com>
2017-09-30 11:49:53 -0600

    Add sln, project, and basic controller

---
+++ src/helloWorld/Controllers/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace helloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloWorld : Controller
+    {
+        // GET api/helloworld
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = new
+            {
+                Data = new
+                {
+                    Message = "Hello, World!"
+                }
+            };
+
+            return this.Json(result);
+        }
+    }
+}
